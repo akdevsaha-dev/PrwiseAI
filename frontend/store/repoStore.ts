20075371019dcb560574c03d3ce4d6006ae108cb
akdevsaha-dev <akdevsaha.dev@gmail.com>
2026-04-03 16:23:25 +0530

    add user and repo store

---
+++ frontend/store/repoStore.ts
@@ -0,0 +1,73 @@
+import { axiosInstance } from "@/utils/instance";
+import toast from "react-hot-toast";
+import { create } from "zustand";
+
+type repo = {
+  id: number;
+  name: string;
+  fullName: string;
+  private: boolean;
+  url: string;
+  language: string | null;
+  issueCount: number;
+};
+
+type Installation = {
+  id: string;
+  installationId: string;
+  accountLogin: string;
+  accountType: string;
+};
+
+type repoStore = {
+  totalRepos: number;
+  isFetchingRepos: boolean;
+  repos: repo[];
+  getRepos: (data: { workspaceId: string }) => void;
+  connected: boolean;
+  installation: Installation | null;
+  isGettingStatus: boolean;
+  getRepoStatus: (data: { workspaceId: string }) => Promise<void>;
+};
+
+export const useRepoStore = create<repoStore>((set) => ({
+  totalRepos: 0,
+  repos: [],
+  isFetchingRepos: false,
+  connected: false,
+  installation: null,
+  isGettingStatus: false,
+  getRepos: async ({ workspaceId }) => {
+    try {
+      set({ isFetchingRepos: true, repos: [] });
+      const res = await axiosInstance.get<{
+        totalRepos: number;
+        repos: repo[];
+      }>(`/v1/repositories/${workspaceId}`);
+      set({ repos: res.data.repos, totalRepos: res.data.totalRepos });
+    } catch (error) {
+      console.error("Error fetching repos", error);
+      toast.error("Failed to fetch repositories!");
+    } finally {
+      set({ isFetchingRepos: false });
+    }
+  },
+  getRepoStatus: async ({ workspaceId }) => {
+    try {
+      set({ isGettingStatus: true });
+      const res = await axiosInstance.get(
+        `/v1/workspace/${workspaceId}/repo-status`,
+      );
+      set({
+        connected: res.data.connected,
+        installation: res.data.installation,
+        isGettingStatus: false,
+      });
+    } catch (error: unknown) {
+      console.error("Error fetching repos", error);
+      toast.error("cannot fetch");
+    } finally {
+      set({ isGettingStatus: false });
+    }
+  },
+}));
---
+++ frontend/store/userStore.ts
@@ -0,0 +1,30 @@
+import { axiosInstance } from "@/utils/instance";
+import { create } from "zustand";
+
+export type User = {
+  id: string;
+  email: string;
+  name: string;
+  image: string | null;
+};
+type userStore = {
+  isGettingUser: boolean;
+  user: User | null;
+  fetchUser: () => Promise<void>;
+};
+
+export const useUserStore = create<userStore>((set) => ({
+  user: null,
+  isGettingUser: false,
+  fetchUser: async () => {
+    set({ isGettingUser: true });
+    try {
+      const res = axiosInstance.get<{ user: User | null }>("/me");
+      set({ user: (await res).data.user, isGettingUser: false });
+    } catch (error) {
+      console.error("Failed to fetch user", error);
+    } finally {
+      set({ user: null, isGettingUser: false });
+    }
+  },
+}));
